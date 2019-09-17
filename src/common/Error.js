9203c819c199ebd4ef04fdfcb2ce6fccc066d25c
Hans Kristian Vik Olsen <hans.kristian.olsen@bouvet.no>
2019-09-17 12:37:28 +0200

    Added table for Viking and Oilers

---
+++ src/common/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Error() {
+  return (
+    <div>
+      Feil url. Bruk enten ingenting etter urelen for Elitserien og Viking,
+      eller /oilers for ishockey
+    </div>
+  );
+}
+
+export default Error;
---
+++ src/components/Oilers/MatchTable.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+function MatchTable(props) {
+  console.log(props.table.standings);
+  const table = props.table.standings;
+  //const standings = props.matches.standings.teamStandings;
+  //const participants = props.matches.participants;
+
+  return (
+    <div>
+      {props.table.standings
+        ? Object.keys(table).map(key => {
+            return (
+              <>
+                <ol>
+                  {table[key].teamStandings.map(team => {
+                    return (
+                      <>
+                        <li>{props.table.participants[team.teamId].name}</li>
+                        <img
+                          src={
+                            props.table.participants[team.teamId].images
+                              .clubLogo.url + "?rule=clip-56x56"
+                          }
+                          alt="fotball"
+                        />
+                      </>
+                    );
+                  })}
+                </ol>
+              </>
+            );
+          })
+        : null}
+    </div>
+  );
+
+  return (
+    <table>
+      <thead>
+        <th>Rank</th>
+        <th>Lag</th>
+      </thead>
+      <tbody>
+        {table
+          ? table.map(table => {
+              return (
+                <tr>
+                  <td>{table.rank}</td>
+                  <td>{table.teamId}</td>
+                </tr>
+              );
+            })
+          : null}
+      </tbody>
+    </table>
+  );
+}
+
+/*
+
+ {props.matches.standings
+          ? Object.keys(standings).map(key => {
+              return (
+                <tr>
+                  <td>{standings[key].rank}</td>
+                  <td>{standings[key].teamId}</td>
+                </tr>
+              );
+            })
+          : null}
+          
+          */
+export default MatchTable;

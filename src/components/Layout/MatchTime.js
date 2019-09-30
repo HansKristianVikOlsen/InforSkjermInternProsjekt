a711bb82cbb719086de44dbbd88c9f94314c6294
Jan <jan.hagevold@bouvet.no>
2019-09-30 08:55:43 +0200

    Add larger font + Source Sans Pro

--- src/components/Layout/MatchTime.js
+++ src/components/Layout/MatchTime.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/styles";
 
+const useStyles = makeStyles(theme => ({
+  text: {
+    fontSize: "24px",
+    fontFamily: "'Source Sans Pro', sans-serif",
+    color: "white"
+  }
+}));
 
 const weekday = new Array(7);
 weekday[0] = "Søndag";
@@ -27,16 +36,20 @@ month[11] = "Des";
 
 
 const MatchTime = ({ date }) => {
+  const classes = useStyles();
 
   return (
-    <Grid container spacing={1} >
-      <Grid item xs={5} sm={3} md={5} lg={4} >
-        {(date.getDate() < 10 ? "0" : "")}{date.getDate()}. {month[date.getMonth()]}
+    <Typography className={classes.text}>
+      <Grid container spacing={1} >
+        <Grid item xs={5} sm={3} md={5} lg={4} >
+          {(date.getDate() < 10 ? "0" : "")}{date.getDate()} {month[date.getMonth()].toUpperCase()}
+
+        </Grid>
+        <Grid item xs={7} sm={9} md={7} lg={8} alignItems="left">
+          {date.getHours()}:{(date.getMinutes() < 10 ? "0" : "")}{date.getMinutes()}
+        </Grid>
       </Grid>
-      <Grid item xs={7} sm={9} md={7} lg={8} alignItems="left">
-        kl. {date.getHours()}:{(date.getMinutes() < 10 ? "0" : "")}{date.getMinutes()}
-      </Grid>
-    </Grid>
+    </Typography>
   )
 };
 
